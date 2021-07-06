c4d81aaba2647b3fe25a4eba60a959244e84dd9c
Revanth-Bhargava <68606029+Revanth-Bhargava@users.noreply.github.com>
2021-07-06 12:18:15 +0530

    Update TabNavigator.js

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { MainStackNavigator, ContactStackNavigator } from "./StackNavigator"; //importing Stack navigator in Tab navigator
 
 const Tab = createBottomTabNavigator();
@@ -8,8 +8,18 @@ const Tab = createBottomTabNavigator();
 const BottomTabNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={MainStackNavigator} />
-      <Tab.Screen name="Contact" component={ContactStackNavigator} />
+      <Tab.Screen name="Home" component={MainStackNavigator} 
+       options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+        ) }} />
+      <Tab.Screen name="Contact" component={ContactStackNavigator} 
+      options={{
+        tabBarLabel: 'Contact',
+        tabBarIcon: ({ color, size }) => (
+            <Ionicons name="people" color={color} size={size} />
+        ) }} />
     </Tab.Navigator>
   );
 };
